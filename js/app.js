abda3c0b2c282382e5a149c71354244d78d26f3c
Maksim Usanov <85723044+maxmillerh@users.noreply.github.com>
2023-06-07 09:49:36 +0300

    вывод записей

--- js/app.js
+++ js/app.js
@@ -23,95 +23,6 @@ for (let elm of elements) {
   observer.observe(elm);
 }
 
-/* показ нахождения на странице */
-
-function ready(fn) {
-  document.addEventListener('DOMContentLoaded', fn, false)
-}
-
-ready(() => {
-  
-  const TableOfContents = {
-    container: document.querySelector('.js-toc'),
-    links: null,
-    headings: null,
-    intersectionOptions: {
-      rootMargin: '0px',
-      threshold: 1
-    },
-    previousSection: null,
-    observer: null,
-
-    init() {
-      this.handleObserver = this.handleObserver.bind(this)
-
-      this.setUpObserver()
-      this.findLinksAndHeadings()
-      this.observeSections()
-
-    },
-
-    handleObserver(entries, observer) {
-
-      entries.forEach(entry => {
-        let href = `#${entry.target.getAttribute('id')}`,
-          link = this.links.find(l => l.getAttribute('href') === href)
-
-
-        if (entry.isIntersecting && entry.intersectionRatio >= 1) {
-          link.classList.add('is-visible')
-          this.previousSection = entry.target.getAttribute('id')
-        } else {
-          link.classList.remove('is-visible')
-        }
-
-        this.highlightFirstActive()
-      })
-    },
-
-    highlightFirstActive() {
-      let firstVisibleLink = this.container.querySelector('.is-visible')
-
-      this.links.forEach(link => {
-        link.classList.remove('is-active')
-      })
-
-      if (firstVisibleLink) {
-        firstVisibleLink.classList.add('is-active')
-      }
-
-/*       if (!firstVisibleLink && this.previousSection) {
-        this.container.querySelector(
-          `a[href="#${this.previousSection}"]`
-        ).classList.add('is-active')
-      } */
-    },
-
-    observeSections() {
-      this.headings.forEach(heading => {
-        this.observer.observe(heading)
-      })
-    },
-
-    setUpObserver() {
-      this.observer = new IntersectionObserver(
-        this.handleObserver,
-        this.intersectionOptions
-      )
-    },
-
-    findLinksAndHeadings() {
-      this.links = [...this.container.querySelectorAll('a')]
-      this.headings = this.links.map(link => {
-        let id = link.getAttribute('href')
-        return document.querySelector(id)
-      })
-    }
-  }
-
-  TableOfContents.init();
-  
-});
 
 
 /* смена блоков */
@@ -315,3 +226,7 @@ permonentnyi_usluga.onclick = function(){
   btnUslPermMicroCor.onclick = function() {
     document.getElementById("procedure2").value = "Перманентный макияж микроблейдинг коррекция";
   }
+
+
+
+	let arrProc = []
---
+++ js/profile.js
@@ -0,0 +1,25 @@
+// появление почты
+
+var mailBtn = document.getElementById('mail_btn');
+var contentMailAdd = document.getElementById('content_mailAdd');
+
+mailBtn.onclick = function() {
+	mailBtn.classList.add('d-none');
+	contentMailAdd.classList.remove('d-none');
+}
+
+
+// Сохранение почты
+var btnMailSave = document.getElementById('btnMailSave');
+btnMailSave.onclick = function() {
+	mailBtn.classList.remove('d-none');
+	contentMailAdd.classList.add('d-none');
+}
+
+// Отмена почты
+var btnMailOtmena = document.getElementById('btnMailOtmena');
+btnMailOtmena.onclick = function() {
+	mailBtn.classList.remove('d-none');
+	contentMailAdd.classList.add('d-none');
+}
+
